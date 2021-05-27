fd2d35d95f123b12ad1c23deeb334a2329bc13c7
SetzerWTFast <natavicenteid@hotmail.com>
2021-05-26 23:02:03 -0300

    reconhecimento de voz aprimorado e fala

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from vosk import Model, KaldiRecognizer
+
 import os
 import pyaudio
+import pyttsx3
+import json
+
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[-2].id)
+
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
 
 model = Model('model')
 rec = KaldiRecognizer(model, 16000)
@@ -18,8 +32,10 @@ while True:
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
-        print(rec.Result())
-    else:
-        print(rec.PartialResult())
+        result = rec.Result()
+        result = json.loads(result)
 
-print(rec.FinalResult())
+        if result is not None:
+            text = result['text']
+            print(text)
+            speak(text)
