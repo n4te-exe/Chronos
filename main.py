339859cbe494f88e7581e331d1f7a6478581584e
SetzerWTFast <natavicenteid@hotmail.com>
2021-05-28 00:09:06 -0300

    realizando reconhecimento de fala e reproducao de hora

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import pyaudio
 import pyttsx3
 import json
+import core
 
 engine = pyttsx3.init()
 
@@ -16,6 +17,8 @@ def speak(text):
     engine.runAndWait()
 
 
+#reconhecimento de fala
+
 model = Model('model')
 rec = KaldiRecognizer(model, 16000)
 
@@ -24,11 +27,12 @@ stream = p.open(format=pyaudio.paInt16,
                 channels=1,
                 rate=16000,
                 input=True,
-                frames_per_buffer=8000)
+                frames_per_buffer=2048)
 stream.start_stream()
 
+#loop do reconhecimento de fala
 while True:
-    data = stream.read(4000)
+    data = stream.read(2048)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
@@ -37,5 +41,8 @@ while True:
 
         if result is not None:
             text = result['text']
+
             print(text)
-            speak(text)
+
+            if text == 'que horas são' or text == 'me diga as horas':
+                speak(core.SystemInfo.get_time())
