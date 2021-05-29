58728a579d99249e634bc9742191ebd660d15980
SetzerWTFast <natavicenteid@hotmail.com>
2021-05-28 23:21:08 -0300

    preparando para reconhecimento das frases-montando banco

---
+++ nlu/model.py
@@ -0,0 +1,8 @@
+import yaml
+
+data = yaml.safe_load(open('nlu\\train.yml', 'r', encoding='utf-8').read())
+
+inputs, outputs = [], []
+
+for command in data['commands']:
+    inputs.append(command['input'])
